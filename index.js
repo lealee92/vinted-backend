dc5249881527ce5a8dd223171a1699257afad5f4
Léa LEE <lealee@MacBook-Pro-de-Lea.local>
2022-08-31 12:42:37 +0200

    confiv vars

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(formidable());
 app.use(cors());
 
 cloudinary.config({
-  cloud_name: "dzo87dcws",
+  cloud_name: "process.env.CLOUDINARY_CLOUD_NAME",
   api_key: "process.env.CLOUDINARY_API_KEY",
   api_secret: "process.env.CLOUDINARY_API_SECRET",
 });
