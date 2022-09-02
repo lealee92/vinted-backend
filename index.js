f729e9bdbe648ee68bf33f9697ac0a5046f835c0
Léa LEE <lealee@MacBook-Pro-de-Lea.local>
2022-09-02 11:00:01 +0200

    port 3000

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.all("*", (req, res) => {
 });
 
 // pour écouter les requêtes du port 3000
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server has started !!");
 });
